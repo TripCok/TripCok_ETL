3514677a8d246456db128676d3c5fea85cad134b
Jeonghoon2 <ukidd12@naver.com>
2024-12-27 16:48:32 +0900

    [FIX] spark resource limit

--- py/common/LogsCleansing.py
+++ py/common/LogsCleansing.py
@@ -40,8 +40,8 @@ class LogsCleansing:
             .appName("Logs Cleansing") \
             .config("spark.jars", ",".join(jar_files)) \
             .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
-            .config("spark.executor.memory", "4g") \
-            .config("spark.executor.cores", "2") \
+            .config("spark.executor.memory", "2g") \
+            .config("spark.executor.cores", "1") \
             .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
             .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
             .getOrCreate()
