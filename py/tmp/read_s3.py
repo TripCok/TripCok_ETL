f0148008db4c0c8c6060d22986dc5506da9152fb
lsiwh37249 <lsiwh37249@gmail.com>
2024-12-31 17:28:06 +0900

    [FEAT]S3 접속하여 dataframe 읽어오는 코드

---
+++ py/tmp/read_s3.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+from common.SparkSess import SessionGen
+
+spark = SessionGen().create_session(app_name="member_place_recommend", local=False)
+
+# S3 Parquet 파일 경로
+s3_path = "s3a://tripcok/processed_data/cre_dtm=2024-12-28/"
+
+# Parquet 파일 읽기
+df = spark.read.parquet(s3_path)
+
+# 데이터 확인
+df.show()
